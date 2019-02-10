5b55729f5bbc4c98244ab6db238feb5c1dd6dfcb
Dan Bal <db3260@columbia.edu>
2019-02-10 17:14:19 +0000

    Basic setup

--- Minx-main.py
+++ Minx-main.py
@@ -1,1 +1,14 @@
-print('Hello Jame')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Main Page")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
