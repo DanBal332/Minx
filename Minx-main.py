26da885d614662d3b6da58ac892872de88ead763
Dan Bal <db3260@columbia.edu>
2019-02-10 18:01:28 +0000

    Basic setup ..

--- Minx-main.py
+++ Minx-main.py
@@ -4,9 +4,29 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html", title="Welcome")
+
+
+@app.route("/detail")
+def detail():
+    return render_template("detail.html", title="Detail")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
+
+
+@app.route("/SignUp")
+def signup():
+    return render_template("signup.html", title="Sign Up")
+
+
 @app.route("/home")
 def home():
-    return render_template("welcome.html", title="Welcome")
+    return render_template("home.html", title="Home")
 
 
 if __name__ == '__main__':
