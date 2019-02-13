f317f8b73994c3392dff45fd7f27ca382e1a20b1
Dan Bal <db3260@columbia.edu>
2019-02-13 21:34:58 +0000

    Completed forms for login and registration

--- Jim/forms.py
+++ Jim/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from Jim.models import User
 
 
+# Class that will serve as form registration of user
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -13,7 +15,20 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # Function that will check whether username has been taken (registration)
+    def validate_username(self, username):
+        user = User.query.filber_by(username=username.data).first()
+        if user:
+            raise ValidationError("This username has been taken, please chose a different username.")
 
+    # Function that will check whether email has been taken (login)
+    def validate_email(self, email):
+        email = User.query.filber_by(email=email.data).first()
+        if email:
+            raise ValidationError("This email has been taken, please chose a different email.")
+
+
+# Class that will serve as form login of user
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
